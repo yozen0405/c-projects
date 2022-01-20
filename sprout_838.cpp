6a4a63c55929fba63319d0e347869daf92a911d5
yozen0405 <71330526+yozen0405@users.noreply.github.com>
2022-01-20 14:49:42 +0800

    Update sprout_838.cpp

--- sprout_838.cpp
+++ sprout_838.cpp
@@ -2,33 +2,39 @@
 #define N 1000000
 #define int long long
 using namespace std;
-int n,c,arr[N],num[11],r_minus[11],ans=0; //num index starts from 0
+int n,c,arr[N],num[11],ans=0; //num[i] 紀錄arr[l]~arr[r] i=1~10 出現的次數
 signed main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cin>>n>>c;
+	int k=0,r; 
     for(int i=0;i<n;i++) cin>>arr[i];
-    for(auto u:arr) num[u]++;
-    for(int i=1;i<=c;i++){
-        if(num[i]==0){
-            cout<<0;
-            return 0;
-        }
-    } 
+    for(int i=0;i<n;i++){
+            if(num[arr[i]]==0){
+                num[arr[i]]++;
+                k++;
+                if(k==c){ //找到1~c最後出現至少一次的數字的index
+                    r=i;
+                    break;
+                }
+            }
+    }
+    if(k!=c){
+        cout<<0;
+        return 0;
+    }
     for(int l=0;l<n;l++){
         if(l>0){
-            num[arr[l-1]]-=1;
-            if(num[arr[l-1]]==0) break; 
-        }
-        for(int i=1;i<=c;i++) r_minus[i]=0;
-        for(int r=n-1;r>=l;r--){
-            if(r!=n-1){
-                r_minus[arr[r+1]]++;
-                if(num[arr[r+1]]-r_minus[arr[r+1]]==0) break;
+            num[arr[l-1]]-=1; //扣掉之前左界包含數字的數量
+            while(r<n-1&&num[arr[l-1]]==0){ //增加右界找目前長度最小的範圍
+                r++;
+                num[arr[r]]++; 
             }
-            ans++;
+            if(num[arr[l-1]]==0) //如果都找不到代表不用繼續了
+                break;
         }
+        ans+=n-r; //(n-1)-r+1
     }
     cout<<ans;
 }
