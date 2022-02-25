1a665e1ba08c57fe912374af96252646ec0e11ed
yozen0405 <71330526+yozen0405@users.noreply.github.com>
2022-02-25 15:03:57 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,31 +1,50 @@
 #include <bits/stdc++.h>
-#define N 1000000
 #define int long long
 using namespace std;
-int n,c,arr[N],num[11],ans=0; //num[i] 紀錄arr[l]~arr[r] i=1~10 出現的次數
-signed main()
-{
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cin>>n>>c;
-	int k=0,r=0; 
-    for(int i=0;i<n;i++) cin>>arr[i];
-    for(int l=0;l<n;l++){
-        if(l>0){
-            num[arr[l-1]]-=1; //扣掉之前左界包含數字的數量
-            if(num[arr[l-1]]==0) k--;
+string n;
+int ans=0;
+int dp[20][12],arr[20],sum[20];//dp[長度][數字]
+const int mod = 1e9 + 7;
+signed main(){
+    //建表
+    dp[1][10]=1;
+    for(int len=1;len<=19;len++){
+        for(int num=9;num>=1;num--){
+            dp[len][num]=dp[len-1][num]+dp[len][num+1];
+            sum[len]+=dp[len][num];
+            //cout<<"dp"<<"("<<len<<","<<num<<"): "<<dp[len][num]<<"\n";
         }
-        while(r<n&&k<c){ //增加右界找目前長度最小的範圍
-            if(num[arr[r]]==0) k++;
-            num[arr[r]]++; 
-            r++;
-        }
-        if(l>0){
-            if(num[arr[l-1]]==0) //如果都找不到代表不用繼續了
-                break;
-        }
-        ans+=n-r+(k==c); //(n-1)-r+1
-        //cout<<"ans:"<<ans<<"\n";
+        
     }
-    cout<<ans<<"\n";
+    while(cin>>n){
+        ans=0;
+        int ind;
+        for(int i=1;i<=n.size();i++){
+            string s=n.substr(i-1,i);
+            arr[n.size()-i+1]=stoll(s);
+            cout<<n.size()-i+1<<","<<arr[n.size()-i+1]<<"\n";
+        }
+        ind=n.size();
+        //cout<<"ind:"<<ind<<"\n";
+        for(int len=1;len<ind;len++){
+            ans+=sum[len];
+            ans%=mod;
+        }
+        for(int i=ind;i>0;i--){ //從高位數讀到低位數
+            int first,previous_num;
+            if(i==ind) previous_num=1;
+            else{
+                previous_num=arr[i+1];
+                if(arr[i]<arr[i+1]) break;
+
+            }
+            if(i==1) first=arr[i];
+            else first=arr[i]-1;
+            for(int j=first;j>=previous_num;j--){
+                ans+=dp[i][j];
+                ans%=mod;
+            }
+        }
+        cout<<ans%mod<<"\n";
+    }
 }
