07201ab256b6ebf12ff3cc935ce2045c171b79bb
yozen0405 <71330526+yozen0405@users.noreply.github.com>
2022-02-25 16:22:59 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -3,12 +3,12 @@
 using namespace std;
 string n;
 int ans=0;
-int dp[20][12],arr[1000001],sum[20];//dp[長度][數字]
+int dp[1000001][12],arr[1000001],sum[1000001];//dp[長度][數字]
 const int mod = 1e9 + 7;
 signed main(){
     //建表
     dp[1][10]=1;
-    for(int len=1;len<=19;len++){
+    for(int len=1;len<1000001;len++){
         for(int num=9;num>=1;num--){
             dp[len][num]=dp[len-1][num]+dp[len][num+1];
             sum[len]+=dp[len][num];
@@ -27,10 +27,12 @@ signed main(){
         }
         for(int i=1;i<=ind;i++){ //從高位數讀到低位數
             int first,previous_num;
-            string s1=n.substr(i-1,i),s2=n.substr(i-2,i-1);
-            int n1=stoll(s1),n2=stoll(s2);
+            string s1=n.substr(i-1,1),s2;
+            int n1=stoll(s1),n2;
             if(i==1) previous_num=1;
             else{
+                s2=n.substr(i-2,i-1);
+                n2=stoll(s2);
                 previous_num=n2;
                 if(s1<s2) break; //現在這項比前一項還小
 
