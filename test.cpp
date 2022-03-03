f6bac9068daacf4d46ff1db7f13d8d476b2fd310
yozen0405 <71330526+yozen0405@users.noreply.github.com>
2022-03-03 16:16:59 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,35 +1,45 @@
 #include <bits/stdc++.h>
 #define int long long
 using namespace std;
-int n,ans=0;
+int n,m,ans=0;
 vector<int> G[1000000];
-int cnt[1000000],hei[1000000],is_root[1000000];
-void dfs(int u,int p){
-    cnt[u]=cnt[p]+1;
+vector<int> ans;
+int cnt[1000000],wei[1000000];
+void rec(int u){
+    int mx_num;
+    if(G[u].empty()){
+        cout<<u<<" ";
+        return;
+    } 
+    mx_num=G[u][0];
     for(auto v:G[u]){
-        if(v==p) continue;
-        dfs(v, u);
+        if(cnt[v]>cnt[mx_num]) mx_num=v;
+    }
+    rec(mx_num);
+}
+void dfs(int u){
+    for(auto v:G[u]){
+        dfs(v);
+        cnt[u]+=cnt[v];
     }
 }
 signed main(){
-    cin>>n;
-    int u,v;
+    cin>>n>>m;
+    int u,l,r;
+    for(int i=n;i<=2*n-1;i++){
+        cin>>cnt[i];
+    }
+    for(int i=0;i<m;i++){
+        cin>>wei[i];
+    }
     for(int i=1;i<=n-1;i++){
-        cin>>u>>v;
-        G[u].push_back(v);
-        G[v].push_back(u);
+        cin>>u>>l>>r;
+        G[u].push_back(l);
+        G[u].push_back(r);
     }
-    dfs(0, 0);
-    int mx=0;
-    for(int i=0;i<n;i++){
-        if(cnt[i]>cnt[mx]) mx=i;
+    for(int i=0;i<m;i++){
+        dfs(1);
+        rec(1);
+        
     }
-    for(int i=0;i<n;i++){
-        cnt[i]=0;
-    }
-    dfs(mx, mx);
-    for(int i=0;i<n;i++){
-        if(cnt[i]>cnt[mx]) mx=i;
-    }
-    cout<<cnt[mx]-1<<"\n";
 }
