0db173723f636a51c1ea10019e0e530ed360559d
yozen0405 <71330526+yozen0405@users.noreply.github.com>
2022-03-11 15:16:24 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,45 +1,41 @@
 #include <bits/stdc++.h>
 #define int long long
 using namespace std;
-int n,m,ans=0;
-vector<int> G[1000000];
-vector<int> ans;
-int cnt[1000000],wei[1000000];
-void rec(int u){
-    int mx_num;
-    if(G[u].empty()){
-        cout<<u<<" ";
-        return;
-    } 
-    mx_num=G[u][0];
-    for(auto v:G[u]){
-        if(cnt[v]>cnt[mx_num]) mx_num=v;
-    }
-    rec(mx_num);
-}
-void dfs(int u){
-    for(auto v:G[u]){
-        dfs(v);
-        cnt[u]+=cnt[v];
-    }
-}
+int n,target,k;
+int arr[1000000];
+map<int,int> mp;
 signed main(){
-    cin>>n>>m;
-    int u,l,r;
-    for(int i=n;i<=2*n-1;i++){
-        cin>>cnt[i];
+    cin>>n>>k;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
     }
-    for(int i=0;i<m;i++){
-        cin>>wei[i];
+    sort(arr, arr+n);
+    int L=-1e18,R=1e18;
+    while(R-L>1){
+        int MID=(L+R)/2;
+        int count=0;
+        for(int i=0;i<n;i++){ // search how many pairs lower than mid
+            if(arr[i]<0){//對於arr[i]有幾個跟他乘會比MID小
+                int l=i, r=n;
+                while(r-l>1){
+                    int mid=(l+r)/2;
+                    if(arr[i]*arr[mid]<=MID) r=mid; //arr[mid]是正
+                    else l=mid;//arr[mid]是負
+                }
+                count+=n-r; //l:跟他乘恰大於MID ，又r=l+1，n-r相當於((n-1)-(l+1))+1，是r~n-1小於MID的區間
+            }
+            else{//因為arr[i]乘的一定是正數或0，所以需要看有幾個正數比MID小
+                int l=i, r=n; 
+                while(r-l>1){
+                    int mid=(l+r)/2;
+                    if(arr[i]*arr[mid]<=MID) l=mid;
+                    else r=mid;
+                }
+                count+=l-i;//l:跟他乘恰大於MID，l-i相當於((l-1)-i)+1
+            }
+        }
+        if(count<k) L=MID;//mid<target
+        else R=MID;
     }
-    for(int i=1;i<=n-1;i++){
-        cin>>u>>l>>r;
-        G[u].push_back(l);
-        G[u].push_back(r);
-    }
-    for(int i=0;i<m;i++){
-        dfs(1);
-        rec(1);
-        
-    }
+    cout<<R;
 }
