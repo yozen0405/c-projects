6058f3431d70274dd5867671b077ec317c819d11
yozen0405 <71330526+yozen0405@users.noreply.github.com>
2022-03-17 13:07:04 +0800

    Update test.cpp

--- tmp/test.cpp
+++ tmp/test.cpp
@@ -3,26 +3,47 @@
 using namespace std;
 int n,k,ans;
 int arr[1000000];
+map<int,int> mp;
 signed main(){
     cin>>n>>k;
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
     int l=0,r=0,cnt=0;
-    while(l!=n){
-        map<int,int> mp;
-        r=l;
-        cnt=0;
+    while(cnt<=k&&r<n){
+            if(!mp[arr[r]]) cnt++;
+            mp[arr[r]]++;
+            r++;
+            cout<<"r:"<<r<<",cnt:"<<cnt<<"\n";
+    }
+    r--;
+    if(cnt>k){
+            mp[r]--;
+            r--;
+            cnt--;
+    }
+     cout<<l<<","<<r<<",cnt:"<<cnt<<"\n";
+    while(l!=n-1){
+        if(mp[l]==1){
+            cnt--;
+            mp[l]=0;
+        }
+        l++;
         while(cnt<=k&&r<n){
             if(!mp[arr[r]]) cnt++;
             mp[arr[r]]++;
             r++;
-            //cout<<"r:"<<r<<",cnt:"<<cnt<<"\n";
+            cout<<"r:"<<r<<",cnt:"<<cnt<<"\n";
         }
-        if(cnt>k) r--;
-       // cout<<l<<","<<r<<"\n";
-        ans+=r-l;
-        l++;
+        r--;
+        if(cnt>k){
+            mp[r]--;
+            r--;
+            cnt--;
+        } 
+        
+       cout<<l<<","<<r<<",cnt:"<<cnt<<"\n";
+        ans+=r-l+1;
     }
     cout<<ans;
 }
