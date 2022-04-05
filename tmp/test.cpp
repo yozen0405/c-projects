8845606e41f2eec2f709e69f4e29c4737ce0b0bf
yozen0405 <71330526+yozen0405@users.noreply.github.com>
2022-04-06 00:34:00 +0800

    Update test.cpp

--- tmp/test.cpp
+++ tmp/test.cpp
@@ -1,27 +1,34 @@
 #include <bits/stdc++.h>
 #define int long long
 using namespace std;
-int n,m;
-string arr[1000001];
-vector<int> G[100001];
-void dfs(int u){
-    cout<<arr[u];
-    for(auto v:G[u]){
-        dfs(v);
-    }
+int t,n;
+int cmp(pair<int,int> a,pair<int,int> b){
+    if(a.first==b.first) return a.second<b.second;
+    return a.first>a.first;
 }
 signed main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-    cin>>n;
-    for(int i=1;i<=n;i++){
-        cin>>arr[i];
+    cin>>t;
+    while(t--){
+        cin>>n;
+        vector<pair<int,int>> vec(n);
+        vector<int> cnt(n);
+        int l,r;
+        for(int i=0;i<n;i++){
+            cin>>vec[i].first>>vec[i].second;
+        }
+        sort(vec.begin(),vec.end(),cmp);
+        int mx=-1e9;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(vec[i].second>=vec[j].first){
+                    cnt[i]++;cnt[j]++;
+                }
+            }
+            mx=max(mx,cnt[i]);
+        }
+        cout<<n-(mx+1)<<"\n";
+
     }
-    int u,v;
-    for(int i=1;i<=n-1;i++){
-        cin>>u>>v;
-        G[u].push_back(v);
-        if(i==n-1) dfs(u);
-    }
-    
 }
