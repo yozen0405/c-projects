412fe8edff2d2716c02d5b69d1f209586c6ddb37
yozen0405 <71330526+yozen0405@users.noreply.github.com>
2022-04-06 14:04:04 +0800

    Update test.cpp

--- tmp/test.cpp
+++ tmp/test.cpp
@@ -8,7 +8,7 @@ signed main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
     cin>>n>>m>>k>>s;
-    for(int i=0;i<n;i++){
+    for(int i=1;i<=n;i++){
         cin>>arr[i];
     }    
     int u,v;
@@ -20,12 +20,13 @@ signed main(){
     for(int i=1;i<=k;i++){
         vector<int> vis(n);
         queue<int> q;
-        for(int j=0;j<n;j++){
-            if(arr[i]==k) vis[i]=1;
-            q.push(i);
+        for(int j=1;j<=n;j++){
+            if(arr[j]==k) vis[j]=1;
+            q.push(j);
         }
+        //bfs
         while(!q.empty()){
-
+            
         }
     }
 }
