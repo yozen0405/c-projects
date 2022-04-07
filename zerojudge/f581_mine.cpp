9a548f6088aff87bb783bdb6e60d2d37dce178be
yozen0405 <71330526+yozen0405@users.noreply.github.com>
2022-04-07 16:31:35 +0800

    Create f581_mine.cpp

---
+++ zerojudge/f581_mine.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+int n,m;
+signed main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin>>n>>m;
+    int a,tot=0;
+    vector<int> pref(n+1);
+    for(int i=1;i<=n;i++){
+      cin>>a;
+      pref[i]=pref[i-1]+a;
+      tot=pref[i];
+    }
+    int p;
+    auto pos=pref.begin()+1;
+    auto prev_pos=pref.begin();
+    while(m--){
+      cin>>p;
+      p%=tot;
+      prev_pos=pos;
+      pos=lower_bound(pref.begin(),pref.end(),*(pos-1)+p);
+      if(pos==pref.end()||pos==pref.end()-1){
+        if(pos==pref.end()) pos-=1;
+        p-=*(pos)-*(prev_pos-1);
+        pos=pref.begin()+1;
+        //cout<<*pos<<endl;
+        if(p>0){
+          pos=lower_bound(pref.begin(),pref.end(),p)+1;
+        } 
+      }
+      else{
+        pos+=1;
+      }
+      
+    }
+    cout<<pos-pref.begin()-1<<"\n";
+    
+}
