9470935c53914d7ef9db078321db2bea2fe02d73
yozen0405 <71330526+yozen0405@users.noreply.github.com>
2022-04-07 16:00:04 +0800

    Update test.cpp

--- tmp/test.cpp
+++ tmp/test.cpp
@@ -1,32 +1,38 @@
 #include <bits/stdc++.h>
 #define int long long
 using namespace std;
-int n,m,k,s,arr[1000000];
-int dis[100001][305];
-vector<int> G[1000000];
+int n,m;
 signed main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-    cin>>n>>m>>k>>s;
+    cin>>n>>m;
+    int a,tot=0;
+    vector<int> pref(n+1);
     for(int i=1;i<=n;i++){
-        cin>>arr[i];
-    }    
-    int u,v;
-    for(int i=0;i<m;i++){
-        cin>>u>>v;
-        G[u].push_back(v);
-        G[v].push_back(u);
+      cin>>a;
+      pref[i]=pref[i-1]+a;
+      tot=pref[i];
     }
-    for(int i=1;i<=k;i++){
-        vector<int> vis(n);
-        queue<int> q;
-        for(int j=1;j<=n;j++){
-            if(arr[j]==k) vis[j]=1;
-            q.push(j);
-        }
-        //bfs
-        while(!q.empty()){
-            
-        }
+    int p;
+    auto pos=pref.begin()+1;
+    auto prev_pos=pref.begin();
+    while(m--){
+      cin>>p;
+      p%=tot;
+      prev_pos=pos;
+      pos=lower_bound(pref.begin(),pref.end(),*(pos-1)+p);
+      if(pos==pref.end()){
+        if(pos==pref.end()) pos-=1;
+        p-=*(pos-1)-*(prev_pos-1);
+        pos=pref.begin()+1;
+        if(p>0){
+          pos=lower_bound(pref.begin(),pref.end(),p)+1;
+        } 
+      }
+      else{
+        pos+=1;
+      }
+      cout<<pos-pref.begin()-1<<"\n";
     }
+    
 }
