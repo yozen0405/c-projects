52729d3fee48e7aa79f348a769756a72e79f53e7
yozen0405 <71330526+yozen0405@users.noreply.github.com>
2022-04-10 16:08:13 +0800

    Update g277.cpp

--- zerojudge/g277.cpp
+++ zerojudge/g277.cpp
@@ -1,31 +1,29 @@
 #include <bits/stdc++.h>
 #define int long long
-#define N 300005
-#define x first
-#define y second
 using namespace std;
-int n,arr[N],pref[N]; //arr unsort, pref sum
-pair<int,int> sorted[N];
-
-signed main()
-{
+int n,pref[1000000],cur=1;
+int cmp(pair<int,int> a,pair<int,int> b){
+  if(a.first==b.first) return a.second<b.second;
+  return a.first<b.first;
+}
+int rec(vector<pair<int,int>>& vec,int l,int r){
+  if(l==r) return pref[r]-pref[l-1];
+  while(l>vec[cur].second||r<vec[cur].second) cur++;
+  int m=vec[cur].second;
+  if(pref[m-1]-pref[l-1]<=pref[r]-pref[m]) l=m+1;
+  else r=m-1;
+  return rec(vec,l,r);
+}
+signed main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
     cin>>n;
+    vector<pair<int,int>> srt(n+1); //value,index
     for(int i=1;i<=n;i++){
-        cin>>sorted[i-1].x;
-        arr[i]=sorted[i-1].x;
-        sorted[i-1].y=i;
-        pref[i]=pref[i-1]+arr[i];
+      cin>>srt[i].first;
+      srt[i].second=i;
+      pref[i]=pref[i-1]+srt[i].first;
     }
-    sort(sorted,sorted+n);
-    int r=n,l=1,ind=0;
-    while(r>l){
-        while(sorted[ind].y>r || sorted[ind].y<l) ind++;
-        int left=pref[sorted[ind].y-1] - pref[l-1];
-        int right=pref[r]-pref[sorted[ind].y];
-        if(left>right) r=sorted[ind].y-1;
-        else l=sorted[ind].y+1;
-    }
-    cout<<arr[l];
+    sort(srt.begin()+1,srt.end(),cmp);
+    cout<<rec(srt,1,n);
 }
