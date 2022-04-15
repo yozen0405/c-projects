359a536e8a55bd0e470259b11e579c3047d796ec
yozen0405 <71330526+yozen0405@users.noreply.github.com>
2022-04-15 15:02:50 +0800

    Update bug.cpp

--- tmp/bug.cpp
+++ tmp/bug.cpp
@@ -1,45 +1,46 @@
 #include <bits/stdc++.h>
 #define int long long
 using namespace std;
-int n;
-struct Node{
-  int ans=0; 
-};
-Node merge(vector<int> &vec,int idx_l,int idx_r,int mid, Node l, Node r){
-  Node ret;
-  ret.ans=r.ans+l.ans;
-  vector<int> a;
-  int j=mid+1;
-  for(int i=idx_l;i<=mid;i++){
-    while(j<=idx_r && vec[i]>vec[j]) a.push_back(vec[j++]);
-    a.push_back(vec[i]);
-    ret.ans+=j-(mid+1);
-  }
-  while(j<=idx_r) a.push_back(vec[j++]);
-  copy(a.begin(),a.end(),vec.begin()+idx_l);
-  return ret;
-}
-Node dc(vector<int> &vec,int l,int r){
-  if(l==r){
-    return {0};
-  }
-  int mid=(l+r)/2;
-  Node left=dc(vec,l,mid);
-  Node right=dc(vec,mid+1,r);
-  return merge(vec, l, r, mid,left, right);
-}
+int h,w;
+char arr[3000][3000];
+int dp[3000][3000][3];
+const int mod=1e9+7;
 signed main(){
-    int t=0;
-    while(scanf("%d",&n) != EOF){
-      if(n==0) break;
-      t++;
-      vector<int> vec(n);
-      for(int i=0;i<n;i++){
-        cin>>vec[i];
-      }
-      Node ans;
-      ans=dc(vec,0,n-1);
-      vec.clear();
-      printf("Case #%d: %d\n",t,ans.ans);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin>>h>>w;
+    for(int i=1;i<=w;i++){
+        for(int j=1;j<=h;j++){
+            cin>>arr[i][j];
+        }
     }
+    if(w>=1&&h>=2&&arr[1][2]=='.')
+        dp[1][2][0]=1;
+    if(w>=2&&h>=1&&arr[2][1]=='.')
+        dp[2][1][1]=1;
+    if(w>=2&&h>=2&&arr[2][2]=='.')
+        dp[2][2][2]=1;
+    for(int i=1;i<=w;i++){
+        for(int j=1;j<=h;j++){
+            if(arr[i][j]=='.'){
+                if((i==1&&j==2)||(i==2&&j==1)){
+                    //cout<<"i:"<<i<<",j:"<<j<<'\n';
+                    //cout<<dp[i][j][0]<<","<<dp[i][j][1]<<","<<dp[i][j][2]<<"\n";
+                     continue;
+                }
+                else if((i==2&&j==2)){
+                    dp[i][j][0]=(dp[i][j-1][0]+dp[i][j-1][1]+dp[i][j-1][2]+dp[i][j-1][0])%mod;
+                    dp[i][j][1]=(dp[i-1][j][0]+dp[i-1][j][1]+dp[i-1][j][2]+dp[i-1][j][1])%mod;
+                }
+                else{
+                    dp[i][j][0]=(dp[i][j-1][0]+dp[i][j-1][1]+dp[i][j-1][2]+dp[i][j-1][0])%mod;
+                    dp[i][j][1]=(dp[i-1][j][0]+dp[i-1][j][1]+dp[i-1][j][2]+dp[i-1][j][1])%mod;
+                    dp[i][j][2]=(dp[i-1][j-1][0]+dp[i-1][j-1][1]+dp[i-1][j-1][2]+dp[i-1][j-1][2])%mod;
+                    //cout<<"i:"<<i<<",j:"<<j<<'\n';
+                    //cout<<dp[i][j][0]<<","<<dp[i][j][1]<<","<<dp[i][j][2]<<"\n";
+                }
+            }
+        }
+    }
+    cout<<(dp[w][h][0]+dp[w][h][1]+dp[w][h][2])%mod;
 }
