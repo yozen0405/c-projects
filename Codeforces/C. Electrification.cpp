93dbdd6c80b8c6a979a1ff3a3102471d01dfae29
yozen0405 <71330526+yozen0405@users.noreply.github.com>
2022-05-02 17:31:22 +0800

    Update C. Electrification.cpp

--- Codeforces/C. Electrification.cpp
+++ Codeforces/C. Electrification.cpp
@@ -6,6 +6,7 @@ int arr[1000000];
 using namespace std;
 bool check(int m){
     for(int i=0;i<=n-k;i++){
+        //m是目前的距離，使對於每一個k段，只有一個最小的k段的距離是m
         if(arr[i+k-1]-arr[i]<=2*m){ //2*m就是arr[i+k-1]~arr[i]最長的距離，也就是第K大的距離，return true代表可以更小(因為有一段區間符合)
             return true;
         }
