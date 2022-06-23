5a3594203d8adfd28de9138e34b1383ae8c5f2be
yozen0405 <71330526+yozen0405@users.noreply.github.com>
2022-06-23 19:55:10 +0800

    Update bug.cpp

--- tmp/bug.cpp
+++ tmp/bug.cpp
@@ -1,46 +1,74 @@
 #include <bits/stdc++.h>
 #define int long long
 using namespace std;
-int h,w;
-char arr[3000][3000];
-int dp[3000][3000][3];
-const int mod=1e9+7;
+
+const int MAXN=20;
+
+struct TwoSAT{
+    static const int MAXv = 2*MAXN;
+    vector<int> GO[MAXv],BK[MAXv],stk;
+    int vis[MAXv];
+    int SC[MAXv];
+    void imply(int u,int v){ // u imply v
+        GO[u].push_back(v);
+        BK[v].push_back(u);
+    }
+    void dfs(int u,vector<int>*G,int sc){
+        vis[u]=1, SC[u]=sc;
+        for (int v:G[u])
+            if (!vis[v])
+                dfs(v,G,sc);
+        if (G==GO)stk.push_back(u);
+    }
+    void scc(int n){
+        memset(vis,0,sizeof(vis));
+        for (int i=0; i<n; i++)if (!vis[i])
+            dfs(i,GO,-1);
+        memset(vis,0,sizeof(vis));
+        int sc=0;
+        while (!stk.empty()){
+            if (!vis[stk.back()])
+                dfs(stk.back(),BK,sc++);
+            stk.pop_back();
+        }
+    }
+};
+
+
 signed main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cin>>h>>w;
-    for(int i=1;i<=w;i++){
-        for(int j=1;j<=h;j++){
-            cin>>arr[i][j];
+    int t;
+    cin>>t;
+    while(t--){
+        int n,m;
+        cin>>n>>m;
+        string s1="",s2="";
+        TwoSAT SAT;
+        for(int i=0;i<m;i++){
+            cin>>s1;
+            cin>>s2;
+            //+ 2*i m 0
+            //- 2*i+1 h 1
+            int p1=((s1[0]=='m') ? 1:0),p2=((s2[0]=='m') ? 1:0);
+            int num1=s1[1]-'1',num2=s2[1]-'1';
+            //cout<<s1<<","<<s2<<","<<num1<<","<<num2<<"\n";
+            SAT.imply(num1*2+p1,num2*2+!p2);
+            SAT.imply(num2*2+p2,num1*2+!p1);
         }
-    }
-    if(w>=1&&h>=2&&arr[1][2]=='.')
-        dp[1][2][0]=1;
-    if(w>=2&&h>=1&&arr[2][1]=='.')
-        dp[2][1][1]=1;
-    if(w>=2&&h>=2&&arr[2][2]=='.')
-        dp[2][2][2]=1;
-    for(int i=1;i<=w;i++){
-        for(int j=1;j<=h;j++){
-            if(arr[i][j]=='.'){
-                if((i==1&&j==2)||(i==2&&j==1)){
-                    //cout<<"i:"<<i<<",j:"<<j<<'\n';
-                    //cout<<dp[i][j][0]<<","<<dp[i][j][1]<<","<<dp[i][j][2]<<"\n";
-                     continue;
-                }
-                else if((i==2&&j==2)){
-                    dp[i][j][0]=(dp[i][j-1][0]+dp[i][j-1][1]+dp[i][j-1][2]+dp[i][j-1][0])%mod;
-                    dp[i][j][1]=(dp[i-1][j][0]+dp[i-1][j][1]+dp[i-1][j][2]+dp[i-1][j][1])%mod;
-                }
-                else{
-                    dp[i][j][0]=(dp[i][j-1][0]+dp[i][j-1][1]+dp[i][j-1][2]+dp[i][j-1][0])%mod;
-                    dp[i][j][1]=(dp[i-1][j][0]+dp[i-1][j][1]+dp[i-1][j][2]+dp[i-1][j][1])%mod;
-                    dp[i][j][2]=(dp[i-1][j-1][0]+dp[i-1][j-1][1]+dp[i-1][j-1][2]+dp[i-1][j-1][2])%mod;
-                    //cout<<"i:"<<i<<",j:"<<j<<'\n';
-                    //cout<<dp[i][j][0]<<","<<dp[i][j][1]<<","<<dp[i][j][2]<<"\n";
-                }
-            }
+        SAT.scc(2*n);
+        bool flg=0;
+        for(int i=0;i<n;i++){
+            if(SAT.SC[2*i]==SAT.SC[2*i+1]) flg=1;
+            //cout<<SAT.SC[2*i]<<","<<SAT.SC[2*i+1]<<"\n";
         }
+        if(flg) cout<<"BAD\n";
+        else cout<<"GOOD\n";
     }
-    cout<<(dp[w][h][0]+dp[w][h][1]+dp[w][h][2])%mod;
 }
+
+/*
+2 4
+h1 m2
+m2 m1
+h1 h2
+m1 h2
+*/
