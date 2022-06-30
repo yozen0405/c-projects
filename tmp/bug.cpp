c700d6d838b79629ac9a02e872efa65de160314b
yozen0405 <71330526+yozen0405@users.noreply.github.com>
2022-06-30 14:39:23 +0800

    Update bug.cpp

--- tmp/bug.cpp
+++ tmp/bug.cpp
@@ -1,118 +1,34 @@
 #include <bits/stdc++.h>
-
+#define int long long
 using namespace std;
 
-string ltrim(const string &);
-string rtrim(const string &);
-vector<string> split(const string &);
+const int INF = 0x3f3f3f3f;
+const int maxn = 2e5+5;
 
-/*
- * Complete the 'equal' function below.
- *
- * The function is expected to return an INTEGER.
- * The function accepts INTEGER_ARRAY arr as parameter.
- */
-
-const int maxn=1e3+5;
-int dp[maxn];
-
-void table(){
-    for(int i=1;i<=maxn;i++){
-        dp[i]=dp[i-1]+1;
-        if(i>=2) dp[i]=min(dp[i],dp[i-2]+1);
-        if(i>=5) dp[i]=min(dp[i],dp[i-5]+1);
-    }
-}
-
-int equal(vector<int> arr) {
-    int n=arr.size();
-    int mi=0x3f3f3f3f,ret=0;
-    for(int val=0;val<1000;val++){
-        for(int i=0;i<n;i++){
-            ret+=dp[arr[i]-val];
-            if(arr[i]-val<0) return mi;
+int maxProfit(int d, vector<int>& prices) {
+    const int INF=0x3f3f3f3f;
+    int n=prices.size();
+    if(2*d>n) d=n>>1;
+    d*=2;
+    vector<vector<int>> dp(d+1,vector<int>(n+1));
+    vector<vector<int>> mx(d+1,vector<int>(n+1,-INF));
+    for(int i=0;i<=n;i++) mx[0][i]=0;
+    int ans=0;
+    for(int k=1;k<=d;k++){
+        for(int i=1;i<=n;i++){
+            dp[k][i]=-INF;
+            if(k>i) continue;
+            dp[k][i]=max(dp[k][i],mx[k-1][i-1]+((k&1) ? -1:1)*prices[i-1]);
+            mx[k][i]=max(mx[k][i-1],dp[k][i]); //k~i
+            ans=max(ans,dp[k][i]);
         }
-        mi=min(ret,mi);
-        ret=0;
     }
-    return mi;
+    return ans;
 }
-
-int main()
-{
-    ofstream fout(getenv("OUTPUT_PATH"));
-
-    string t_temp;
-    getline(cin, t_temp);
-
-    int t = stoi(ltrim(rtrim(t_temp)));
-    
-    table();
-    
-    for (int t_itr = 0; t_itr < t; t_itr++) {
-        string n_temp;
-        getline(cin, n_temp);
-
-        int n = stoi(ltrim(rtrim(n_temp)));
-
-        string arr_temp_temp;
-        getline(cin, arr_temp_temp);
-
-        vector<string> arr_temp = split(rtrim(arr_temp_temp));
-
-        vector<int> arr(n);
-
-        for (int i = 0; i < n; i++) {
-            int arr_item = stoi(arr_temp[i]);
-
-            arr[i] = arr_item;
-        }
-
-        int result = equal(arr);
-
-        fout << result << "\n";
-    }
-
-    fout.close();
-
-    return 0;
-}
-
-string ltrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        s.begin(),
-        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
-    );
-
-    return s;
-}
-
-string rtrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-        s.end()
-    );
-
-    return s;
-}
-
-vector<string> split(const string &str) {
-    vector<string> tokens;
-
-    string::size_type start = 0;
-    string::size_type end = 0;
-
-    while ((end = str.find(" ", start)) != string::npos) {
-        tokens.push_back(str.substr(start, end - start));
-
-        start = end + 1;
-    }
-
-    tokens.push_back(str.substr(start));
-
-    return tokens;
+signed main(){
+    int n,k;
+    cin>>n>>k;
+    vector<int> a(n);
+    for(int i=0;i<n;i++) cin>>a[i];
+    cout<<maxProfit(k,a);
 }
