9e67f23e725895bdea96ac09c68b566b2a4c13bc
yozen0405 <71330526+yozen0405@users.noreply.github.com>
2022-07-01 17:24:17 +0800

    Update bug.cpp

--- tmp/bug.cpp
+++ tmp/bug.cpp
@@ -1,4 +1,4 @@
-#define lowbit(x) x & (-x)
+#define lowbit(x) (x & (-x))
 class Solution {
 public:
     bool canPartitionKSubsets(vector<int>& a, int k) {
@@ -9,13 +9,14 @@ public:
             tar/=k;
             vector<int> sum(1<<n);
             for(int U=1;U<(1<<n);U++){
-                sum[U]=sum[U-lowbit(U)]+a[log2(lowbit(U))];
+                int idx = __builtin_ctz(lowbit(U));
+                sum[U]=sum[U-lowbit(U)]+a[idx];
             }
             vector<int> dp(1<<n);
             dp[0]=true;
             for(int U=1;U<(1<<n);U++){
                 for(int S=U;S>0;S=(S-1)&U){
-                    cout<<S<<"\n";
+                    //cout<<S<<"\n";
                     if(sum[S]==tar&&dp[U^S]){
                         dp[U]=true;
                         break;
