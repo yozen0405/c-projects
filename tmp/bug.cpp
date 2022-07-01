ae2decb3872f914b80328acb284e24725051c7e3
yozen0405 <71330526+yozen0405@users.noreply.github.com>
2022-07-01 17:13:23 +0800

    Update bug.cpp

--- tmp/bug.cpp
+++ tmp/bug.cpp
@@ -3,24 +3,25 @@ class Solution {
 public:
     bool canPartitionKSubsets(vector<int>& a, int k) {
         int n = a.size();
-        int tar=0;
-        for(auto ele:a) tar+=ele;
-        if(tar%k) return 0;
-        tar/=k;
-        vector<int> sum(1<<n);
-        for(int U=1;U<(1<<n);U++){
-            sum[U]=sum[U-lowbit(U)]+a[log2(lowbit(U))];
-        }
-        vector<int> dp(1<<n);
-        dp[0]=true;
-        for(int U=1;U<(1<<n);U++){
-            for(int S=U;S>=0;S=(S-1)&U){
-                if(S==tar&&dp[U^S]){
-                    dp[U]=true;
-                    break;
+            int tar=0;
+            for(auto ele:a) tar+=ele;
+            if(tar%k) return 0;
+            tar/=k;
+            vector<int> sum(1<<n);
+            for(int U=1;U<(1<<n);U++){
+                sum[U]=sum[U-lowbit(U)]+a[log2(lowbit(U))];
+            }
+            vector<int> dp(1<<n);
+            dp[0]=true;
+            for(int U=1;U<(1<<n);U++){
+                for(int S=U;S>0;S=(S-1)&U){
+                    cout<<S<<"\n";
+                    if(sum[S]==tar&&dp[U^S]){
+                        dp[U]=true;
+                        break;
+                    }
                 }
             }
-        }
-        return dp[(1<<n)-1];
+            return dp[(1<<n)-1];
     }
 };
