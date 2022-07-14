fbc4195ee76f09b209e61a7292626f59d7081160
yozen0405 <71330526+yozen0405@users.noreply.github.com>
2022-07-14 13:40:13 +0800

    Create A. Book.cpp

---
+++ Codeforces/A. Book.cpp
@@ -0,0 +1,77 @@
+// https://codeforces.com/contest/1572/problem/A
+#include <bits/stdc++.h>
+#define int long long
+#define pii pair<int,int>
+#define pb push_back
+using namespace std;
+
+const int INF = 0x3f3f3f3f;
+const int M = 1e9 + 7;
+const int maxn = 2e5 + 5;
+
+int n;
+vector<pii> G[maxn];
+int in[maxn];
+
+vector<int> topo() {
+    queue<int> q;
+    vector<int> vec;
+    for (int i = 1; i <= n; i++){
+        if(!in[i]) {
+            q.push(i); 
+            vec.push_back(i);
+        }
+    }
+    while (q.size()) {
+        int u = q.front();
+        q.pop();
+        for (auto [v, w] : G[u]) {
+            in[v]--;
+            if(!in[v]){
+                q.push(v); 
+                vec.push_back(v);
+            }
+        }
+    }
+    return vec;
+}
+
+int solve(vector<int>& tp) {
+    vector<int> dp(n + 1);
+    int ret = 0;
+    for (auto u : tp) {
+        for (auto [v, w] : G[u]) {
+            dp[v] = max(dp[v], dp[u] + w);
+            ret = max(ret, dp[v]);
+        }
+    }
+    return ret;
+}
+
+signed main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        cin >> n;
+        for (int i = 1; i <= n; i++) {
+            G[i].clear();
+            in[i] = 0;
+        }
+        for (int i = 1, u, v; i <= n; i++) {
+            int m;
+            cin >> m;
+            while (m--) {
+                cin >> v;
+                G[v].pb(make_pair(i, (i < v)));
+                in[i]++;
+            }
+        }
+        vector<int> tp = topo();
+        if (tp.size() != n){
+            cout << -1 << "\n";
+            continue;
+        }
+        cout << solve(tp) + 1 << "\n";
+
+    }
+}
