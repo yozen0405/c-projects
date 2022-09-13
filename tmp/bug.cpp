72a98cc40836815f58f1bce6e49e95c5b9bfe0b2
yozen0405 <71330526+yozen0405@users.noreply.github.com>
2022-09-13 14:00:29 +0800

    Update bug.cpp

--- tmp/bug.cpp
+++ tmp/bug.cpp
@@ -1,114 +1,75 @@
-#include <bits/stdc++.h>
-#define int long long
-#define pii pair<int, int>
-#define mk make_pair
-#define pb push_back
-#define x first
-#define y second
+#include <iostream>
+#include <vector>
+#include <queue>
 using namespace std;
-using P = pair<long long,long long>;
  
-int n;
-vector<P> p;
-
-P operator+ (P a, P b) {
-    return {a.x + b.x, a.y + b.y};
-}
-P operator- (P a, P b) {
-    return {a.x - b.x, a.y - b.y};
-}
-P operator* (P a, int d) {
-    return {a.x * d, a.y * d};
-}
-double dot (P a, P b) {
-    return a.x * b.x + a.y * b.y;
-}
-long long cross (P a, P b) {
-    return a.x * b.y - a.y * b.x;
+const int maxn = 5005;
+ 
+int n, m, a, b, ans, p[maxn], cnt[maxn], in[maxn];
+vector <int> G[maxn], v[maxn];
+queue <int> q;
+ 
+int pa(int x){
+    if (p[x] < 0) return x;
+    else return p[x] = pa(p[x]);
 }
  
-int sign (int x) {
-    if (x < 0) return -1;
-    if (x > 0) return 1;
-    return 0;
-}
- 
-bool oneseg (P a, P b, P c) {
-    if (cross(b - a, c - a) != 0) return false;
-    if (dot(b - a, c - a) < 0) return false;
-    if (dot(a - b, c - b) < 0) return false;
-    return true;
-}
- 
-bool insert (P a, P b, P c, P d) {
-    int c1 = sign(cross(c - a, b - a)) * sign(cross(d - a, b - a));
-    int c2 = sign(cross(b - c, d - c)) * sign(cross(a - c, d - c));
-    if (c1 == 1 || c2 == 1) return false;
-    if (c1 == -1 && c2 == -1) return true;
-    if (oneseg(a, b, c)) return true;
-    if (oneseg(a, b, d)) return true;
-    if (oneseg(c, d, a)) return true;
-    if (oneseg(c, d, b)) return true;
-    return false;
-}
- 
-int area(vector<P> &a) {
-    int ret = 0 ;
-    for (int i = 1; i < a.size(); i++) {
-        ret += cross(a[i - 1], a[i]);
+int main() {
+    cin >> n >> m;
+    for (int i = 0; i < m; i++){
+        cin >> a >> b;
+        G[a].push_back(b);
+        G[b].push_back(a);
+        cnt[a]++;
+        cnt[b]++;
     }
-    ret += cross(a[a.size()- 1], a[0]);
-    return abs(ret);
-}
-
-vector<P> convex () {
-    sort(p.begin(), p.end(), [](P a, P b){ if(a.x == b.x) return a.y > b.y; return a.x < b.x; });
-    vector<int> stk(n);
-    vector<int> use(n);
-    int tp = 0;
-    stk[tp++] = 0;
-    for (int i = 1; i < n; i++) {
-        while (tp >= 2 && sign(cross(p[stk[tp - 1]] - p[stk[tp - 2]], p[i] - p[stk[tp - 1]])) < 0) {
-            use[stk[tp - 1]] = 0;
-            tp--;
-        }
-        use[i] = 1;
-        stk[tp++] = i;
-    }
-    for (int i = n - 2; i >= 0; i--) {
-        if(!use[i]) {
-            while (tp >= 2 && sign(cross(p[stk[tp - 1]] - p[stk[tp - 2]], p[i] - p[stk[tp - 1]])) < 0) {
-                use[stk[tp - 1]] = 0;
-                tp--;
+	// topo 排序
+    for (int i = 1; i <= n; i++){
+		// in_deg == i 的
+        for (int j = 1; j <= n; j++){
+            if (cnt[j] == i){
+                q.push(j);
+                in[j] = 1; // 紀錄是否已經在 vector 裡面
             }
-            use[i] = 1;
-            stk[tp++] = i;
+        }
+        while (!q.empty()){
+            int now = q.front();
+            q.pop();
+            v[i].push_back(now);
+			// 拔點 (now)
+            for (auto v:G[now]){
+                cnt[v]--;
+                if (in[v]) continue;
+                if (cnt[v] <= i){
+                    q.push(v);
+                    in[v] = 1;
+                }
+            }
         }
     }
-    vector<P> h;
-    for (int i = 0; i < tp; i++) {
-        h.pb(p[stk[i]]);
+    for (int i = n; i >= 1; i--){
+		cout << "in: " << i << "\n";
+        for (auto v:v[i]){
+			// 至少有 i 個邊連到比自己 in_deg 大於等於的 node
+			cout << v << " ";
+            p[v] = -1;
+            for (auto k : G[v]){
+                if (!p[k]) continue; 
+				// 如果 in_deg 比自己低的就不跑
+                int x = pa(v), y = pa(k);
+                if (x != y){
+                    if (x > y) swap(x, y);
+                    p[x] += p[y];        
+                    p[y] = x;        
+                }  
+            }
+        }
+		if(v[i].size()) cout << "\n";
+        for (int j = 1; j <= n; j++){
+            if (p[j] < 0) ans = max(ans, -p[j]*i);
+			// p[i] 看他的區塊有多少個點
+			// p[j] -> Di, i -> |S|
+        }
     }
-    return h;
-}
-
-void init() {
-	cin >> n;
-    p.resize(n);
-    for (int i = 0; i < n; i++) cin >> p[i].x >> p[i].y;
-}
-
-void solve() {
-	int cnt = 0;
-	for (int i = 1; i < n; i++) {
-		cnt += __gcd(abs(p[i].x - p[i - 1].x),abs(p[i].y - p[i - 1].y));
-	}
-	// https://i.imgur.com/vhmXuf5.png
-	cnt += __gcd(abs(p[n - 1].x - p[0].x),abs(p[n - 1].y - p[0].y));
-	cout << (area(p) - cnt + 2) / 2 << " " << cnt << "\n";
-}
- 
-signed main () {
-	init();
-	solve();
+    cout << ans << "\n";
 }
