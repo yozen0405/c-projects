0c9d0ce1a8d82c464c6782026b24ca78ee054ca5
yozen0405 <71330526+yozen0405@users.noreply.github.com>
2022-10-26 14:45:09 +0800

    Update f1_stimulation.cpp

--- mine/f1_stimulation.cpp
+++ mine/f1_stimulation.cpp
@@ -7,31 +7,51 @@
 
 using namespace std;
 
-map<string, int> total_point = {{"Ver",0},{"Ham",0},{"Bot",0},{"Per",0},{"Sai",0},{"Nor",0},{"Lec",0},{"Ric",0},{"Gas",0},{"Alo",0},{"Oco",0},{"Vet",0},{"Str",0},{"Tsu",0},{"Rus",0},{"Rai",0},{"Lat",0},{"Gio",0},{"Sch",0},{"Maz",0}};
-map<string, int> track_base_arr = {{"賽道1", 9}, {"賽道2", 9},{"賽道3", 10},{"賽道4", 5},{"賽道5", 4},
-{"賽道6", 4},{"賽道7", 2},{"賽道8", 8},{"賽道9", 4},{"賽道10", 9},
-{"賽道11", 1},{"賽道12", 5},{"賽道13", 3},{"賽道14", 4},{"賽道15", 7},
-{"賽道16", 5},{"賽道17", 6},{"賽道18", 3},
-{"賽道19", 2},{"賽道20", 6}};
+//隨機樹使用參數為時間(毫秒)
+//梅克森旋轉演算法
+unsigned seed = chrono::steady_clock::now().time_since_epoch().count();
+mt19937_64 rng(seed);
+//平均分布
+uniform_int_distribution<int> dis(1,1000000);
 
-string track_name[20] = {};
+map<string, int> total_point = {{"Ver",0},{"Ham",0},{"Bot",0},{"Vip",0},{"Sai",0},{"Nor",0},{"Lec",0},{"Ric",0},{"Gas",0},{"Alo",0},{"Oco",0},{"Vet",0},{"Str",0},{"Tsu",0},{"Rus",0},{"Rai",0},{"Lat",0},{"Gio",0},{"Sch",0},{"Maz",0}};
+map<string, int> track_base_arr = {{"巴林", 3},{"吉達", 4},{"澳洲", 7}, {"伊莫拉", 7},{"邁阿密", 5},{"巴薩隆納", 7},{"摩納哥",10},
+{"巴庫", 3},{"加拿大", 3},{"銀石賽道", 7},{"奧地利", 8},{"法國", 5},{"匈牙利",9},{"比利時", 4},{"荷蘭", 8},{"蒙札", 1},{"新加玻", 9},
+{"日本", 6},{"美國", 6},{"墨西哥", 10},{"巴西", 8},{"阿布札比", 5}};
+
+string track_name[23] = {};
 int times;
-vector<vector<pair<string,int>>> driver={{{"Ver",130},{"Ham",214},{"Bot",126},{"Per",60},{"Sai",36},{"Nor",99},{"Lec",34},{"Ric",71},{"Gas",46},{"Alo",36},{"Oco",34},{"Vet",17},{"Str",13},{"Tsu",14},{"Rus",28},{"Rai",15},{"Lat",12},{"Gio",5},{"Sch",6},{"Maz",4}},
-    {{"Ver",143},{"Ham",201},{"Bot",119},{"Per",67},{"Sai",56},{"Nor",87},{"Lec",54},{"Ric",63},{"Gas",46},{"Alo",36},{"Oco",34},{"Vet",17},{"Str",13},{"Tsu",14},{"Rus",28},{"Rai",15},{"Lat",12},{"Gio",5},{"Sch",6},{"Maz",4}},
-    {{"Ver",156},{"Ham",189},{"Bot",111},{"Per",74},{"Sai",66},{"Nor",75},{"Lec",64},{"Ric",55},{"Gas",46},{"Alo",36},{"Oco",34},{"Vet",17},{"Str",13},{"Tsu",14},{"Rus",28},{"Rai",15},{"Lat",12},{"Gio",5},{"Sch",6},{"Maz",4}},
-    {{"Ver",170},{"Ham",176},{"Bot",104},{"Per",80},{"Sai",69},{"Nor",73},{"Lec",66},{"Ric",52},{"Gas",46},{"Alo",36},{"Oco",34},{"Vet",17},{"Str",13},{"Tsu",14},{"Rus",28},{"Rai",15},{"Lat",12},{"Gio",5},{"Sch",6},{"Maz",4}},
-    {{"Ver",177},{"Ham",170},{"Bot",100},{"Per",83},{"Sai",71},{"Nor",70},{"Lec",69},{"Ric",50},{"Gas",46},{"Alo",36},{"Oco",34},{"Vet",23},{"Str",17},{"Tsu",14},{"Rus",21},{"Rai",15},{"Lat",9},{"Gio",5},{"Sch",6},{"Maz",4}},
-    {{"Ver",177},{"Ham",170},{"Bot",100},{"Per",83},{"Sai",71},{"Nor",70},{"Lec",69},{"Ric",50},{"Gas",46},{"Alo",36},{"Oco",34},{"Vet",23},{"Str",17},{"Tsu",14},{"Rus",21},{"Rai",15},{"Lat",9},{"Gio",5},{"Sch",6},{"Maz",4}},
-    {{"Ver",183},{"Ham",164},{"Bot",96},{"Per",87},{"Sai",74},{"Nor",67},{"Lec",71},{"Ric",48},{"Gas",54},{"Alo",42},{"Oco",38},{"Vet",23},{"Str",17},{"Tsu",16},{"Rus",21},{"Rai",15},{"Lat",9},{"Gio",5},{"Sch",6},{"Maz",4}},
-    {{"Ver",197},{"Ham",151},{"Bot",89},{"Per",93},{"Sai",76},{"Nor",64},{"Lec",74},{"Ric",46},{"Gas",62},{"Alo",42},{"Oco",38},{"Vet",23},{"Str",17},{"Tsu",18},{"Rus",21},{"Rai",15},{"Lat",9},{"Gio",5},{"Sch",6},{"Maz",4}},
-    {{"Ver",210},{"Ham",139},{"Bot",81},{"Per",100},{"Sai",86},{"Nor",52},{"Lec",84},{"Ric",38},{"Gas",62},{"Alo",42},{"Oco",38},{"Vet",23},{"Str",17},{"Tsu",18},{"Rus",14},{"Rai",23},{"Lat",6},{"Gio",7},{"Sch",6},{"Maz",4}},
-    {{"Ver",224},{"Ham",126},{"Bot",74},{"Per",106},{"Sai",96},{"Nor",40},{"Lec",94},{"Ric",30},{"Gas",62},{"Alo",42},{"Oco",38},{"Vet",23},{"Str",17},{"Tsu",18},{"Rus",14},{"Rai",23},{"Lat",6},{"Gio",7},{"Sch",6},{"Maz",4}}};
+string fastest_lap_string;
+vector<vector<pair<string,int>>> driver={{{"Lec",130},{"Ver",214},{"Vip",126},{"Sai",60},{"Rus",36},{"Nor",99},{"Ham",34},{"Ric",71},{"Gas",46},{"Alo",36},{"Oco",34},{"Vet",17},{"Str",13},{"Tsu",14},{"Alb",28},{"Bot",15},{"Lat",12},{"Zho",5},{"Sch",6},{"Mag",4}},
+    {{"Lec",143},{"Ver",201},{"Vip",119},{"Sai",67},{"Rus",56},{"Nor",87},{"Ham",54},{"Ric",63},{"Gas",46},{"Alo",31},{"Oco",29},{"Vet",17},{"Str",13},{"Tsu",14},{"Alb",21},{"Bot",15},{"Lat",9},{"Zho",5},{"Sch",6},{"Mag",4}},
+    {{"Lec",156},{"Ver",189},{"Vip",111},{"Sai",74},{"Rus",66},{"Nor",75},{"Ham",64},{"Ric",55},{"Gas",46},{"Alo",31},{"Oco",29},{"Vet",17},{"Str",13},{"Tsu",14},{"Alb",21},{"Bot",15},{"Lat",9},{"Zho",5},{"Sch",6},{"Mag",4}},
+    {{"Lec",170},{"Ver",176},{"Vip",104},{"Sai",80},{"Rus",69},{"Nor",73},{"Ham",66},{"Ric",52},{"Gas",46},{"Alo",31},{"Oco",29},{"Vet",17},{"Str",13},{"Tsu",14},{"Alb",21},{"Bot",15},{"Lat",9},{"Zho",5},{"Sch",6},{"Mag",4}},
+    {{"Lec",177},{"Ver",170},{"Vip",100},{"Sai",83},{"Rus",71},{"Nor",70},{"Ham",69},{"Ric",50},{"Gas",46},{"Alo",31},{"Oco",29},{"Vet",17},{"Str",13},{"Tsu",14},{"Alb",21},{"Bot",15},{"Lat",9},{"Zho",5},{"Sch",6},{"Mag",4}},
+    {{"Lec",177},{"Ver",170},{"Vip",100},{"Sai",83},{"Rus",71},{"Nor",70},{"Ham",69},{"Ric",50},{"Gas",46},{"Alo",31},{"Oco",29},{"Vet",17},{"Str",13},{"Tsu",14},{"Alb",21},{"Bot",15},{"Lat",9},{"Zho",5},{"Sch",6},{"Mag",4}},
+    {{"Lec",183},{"Ver",164},{"Vip",96},{"Sai",87},{"Rus",74},{"Nor",67},{"Ham",71},{"Ric",48},{"Gas",46},{"Alo",36},{"Oco",34},{"Vet",23},{"Str",17},{"Tsu",14},{"Alb",14},{"Bot",6},{"Lat",6},{"Zho",4},{"Sch",6},{"Mag",4}},
+    {{"Lec",197},{"Ver",151},{"Vip",89},{"Sai",93},{"Rus",76},{"Nor",64},{"Ham",74},{"Ric",46},{"Gas",56},{"Alo",36},{"Oco",34},{"Vet",17},{"Str",13},{"Tsu",14},{"Alb",14},{"Bot",6},{"Lat",6},{"Zho",4},{"Sch",6},{"Mag",4}},
+    {{"Lec",210},{"Ver",139},{"Vip",81},{"Sai",100},{"Rus",86},{"Nor",52},{"Ham",84},{"Ric",38},{"Gas",56},{"Alo",36},{"Oco",34},{"Vet",17},{"Str",13},{"Tsu",14},{"Alb",6},{"Bot",15},{"Lat",4},{"Zho",5},{"Sch",6},{"Mag",4}},
+    {{"Lec",224},{"Ver",126},{"Vip",74},{"Sai",106},{"Rus",96},{"Nor",40},{"Ham",94},{"Ric",30},{"Gas",56},{"Alo",36},{"Oco",34},{"Vet",17},{"Str",13},{"Tsu",14},{"Alb",6},{"Bot",15},{"Lat",4},{"Zho",5},{"Sch",6},{"Mag",4}}};
+
 vector<pair<string,int>> fastest_lap_odd(20);
 vector<pair<string,int>> position(20);
 vector<string> race_position(20);
 vector<pair<string,int>> point(20);
-vector<pair<string,int>> constructor={{"Mercedes",0},{"Red_bull",0},{"Ferrari",0},{"Mclaren",0},{"Alpine",0},
+vector<pair<string,int>> round_point(20);
+vector<int> fastest_lap_odd_calc;
+vector<pair<string,int>> sort_pair(20);
+vector<pair<string,int>> constructor={{"Ferrari",0},{"Red_bull",0},{"Mercedes",0},{"Mclaren",0},{"Alpine",0},
 {"Alpha_tauri",0},{"Aston_Martin",0},{"Williams",0},{"Alfa_romeo",0},{"Haas",0}};
+vector<vector<string>> team = {{"Lec" ,"Sai"},{"Ver" ,"Vip"},{"Rus","Ham"},{"Nor","Ric"},{"Oco","Alo"},
+{"Gas","Tsu"},{"Vet","Str"},{"Alb","Lat"},{"Bot","Zho"},{"Mag","Sch"}};
+int cmp(pair<string,int> a,pair<string,int> b){
+  return a.second > b.second;
+}
+
+long long RandomNumber(){
+    return dis(rng);
+}
+
 void quali(string str){
      int odd = track_base_arr[track_name[times]]-1;
     vector<pair<string,int>> tmp(20);
@@ -45,13 +65,12 @@ void quali(string str){
     }
     int k=0;
     for(int i=0; i < driver[odd].size(); i++){
-        srand( time(NULL) );
+
         int min = 1;
         int max = sum;
         bool is_repeat;
         do{
-            ran = rand() % (max - min + 1) + min;
-            //cout<<ran<<"\n";
+            ran = RandomNumber() % (max - min + 1) + min;
             is_repeat = false;
             for(int j=0;j<indent.size();j++){
                 if(ran <= indent[j].second && ran>= indent[j].first)
@@ -90,12 +109,12 @@ void race(){
     }
     int k=0;
     for(int i=0; i < driver[odd].size(); i++){
-        srand( time(NULL) );
+
         int min = 1;
         int max = sum;
         bool is_repeat;
         do{
-            ran = rand() % (max - min + 1) + min;
+            ran = RandomNumber() % (max - min + 1) + min;
             is_repeat = false;
             for(int j=0;j<indent.size();j++){
                 if(ran <= indent[j].second && ran>= indent[j].first)
@@ -113,6 +132,25 @@ void race(){
     }
 
 }
+
+void match () {
+    vector<pair<string,int>> ve(10);
+    for (int i = 0; i < constructor.size(); i++) {
+        int sum = 0;
+        ve[i].first = constructor[i].first;
+        for (int j = 0; j < team[i].size(); j++) {
+            ve[i].second += total_point[team[i][j]];
+        }
+    }
+    sort (ve.begin(), ve.end(), [](pair<string,int> a, pair<string, int> b) {return a.second > b.second; });
+    cout << "Round" << times+1 << "之後的車隊積分榜" << "\n";
+    for (int i = 0; i < ve.size(); i++) {
+        cout << i+1 << ":" << ve[i].first << ":" << ve[i].second << "\n";
+    }
+}
+
+
+
 void point_system(){
     for(int i=0; i< race_position.size();i++){
         if (i==0){
@@ -164,36 +202,80 @@ void point_system(){
         point[j].first = race_position[j];
     }
 }
+
+void round_point_system(){
+    for(int i=0; i< race_position.size();i++){
+        if (i==0){
+            round_point[i].first = race_position[i];
+            round_point[i].second = 25;
+        }
+        else if (i==1){
+            round_point[i].first = race_position[i];
+            round_point[i].second = 18;
+        }
+        else if (i==2){
+            round_point[i].first = race_position[i];
+            round_point[i].second = 15;
+        }
+        else if (i==3){
+            round_point[i].first = race_position[i];
+            round_point[i].second = 12;
+        }
+        else if (i==4){
+            round_point[i].first = race_position[i];
+            round_point[i].second = 10;
+        }
+        else if (i==5){
+            round_point[i].first = race_position[i];
+            round_point[i].second = 8;
+        }
+        else if (i==6){
+            round_point[i].first = race_position[i];
+            round_point[i].second = 6;
+        }
+        else if (i==7){
+            round_point[i].first = race_position[i];
+            round_point[i].second = 4;
+        }
+        else if (i==8){
+            round_point[i].first = race_position[i];
+            round_point[i].second = 2;
+        }
+        else if (i==9){
+            round_point[i].first = race_position[i];
+            round_point[i].second = 1;
+        }
+        else{
+            round_point[i].second = 0;
+        }
+
+    }
+    for(int j=10; j<race_position.size();j++){
+        round_point[j].first = race_position[j];
+    }
+
+}
 void fastest_lap(){
     int sum = 0;
     for(int i=0; i<race_position.size();i++){
         fastest_lap_odd[i].first = race_position[i];
     }
     for(int i=0; i<race_position.size();i++){
-        if (i<=4){
-            fastest_lap_odd[i].second = sum+10;
-            sum+=10;
+        fastest_lap_odd[i].second = sum+5;
+        sum+=5;
         }
-        else if (i>=10){
-            fastest_lap_odd[i].second = sum+1;
-            sum++;
-        }
-        else{
-            fastest_lap_odd[i].second = 8+sum;
-            sum+=8;
-        }
-    }
+
     int ran;
-        srand( time(NULL) );
         int min = 1;
         int max = 100;
-        ran = rand() % (max - min + 1) + min;
+        ran = RandomNumber() % (max - min + 1) + min;
         for(int j=0;j<fastest_lap_odd.size();j++){
             if(fastest_lap_odd[j].second >= ran){
                 if(j<10){
                     point[j].second += 1;
                 }
-                break;
+            fastest_lap_string = point[j].first;
+            break;
             }
         }
 }
@@ -203,33 +285,53 @@ void clear_all(){
     race_position.clear();
     position.resize(20);
     fastest_lap_odd.resize(20);
+    fastest_lap_string=' ';
     race_position.resize(20);
     point.clear();
     point.resize(20);
+    round_point.clear();
+    round_point.resize(20);
+    //for (auto i = total_point.begin(); i != total_point.end(); i++) {
+        //i->second = 0;
+    //}
 }
 void print_quali(){
+    cout << "----------------------------------\n";
+    cout << "Round:" << times+1 << "   " << "賽道: " << track_name[times] <<"    " << "排位賽" << "\n";
     for(int i=0; i<position.size();i++){
         cout << i+1 << "." << position[i].first<<"\n";
     }
+    cout << "\n";
 }
 void print_race(){
+    cout << "Round:" << times+1 << "   " << "賽道: " << track_name[times] << "    " << "正賽" << "\n";
     for(int i=0; i<race_position.size();i++){
-        cout << i+1 << "." << race_position[i]<<"\n";
+        cout << i+1 << "." << race_position[i]<<"    "<< point[i].second <<"\n";
     }
+    cout << "最速單圈:" << fastest_lap_string << "\n";
+    cout << "\n";
 }
+
 void print_driver_standing(){
+    cout << "Round" << times+1 << "之後的車手積分榜" << "\n";
     for(int i=0; i<point.size();i++)
-        cout << i+1 << "." << point[i].first << "  " << total_point[point[i].first]<<"\n";
+        cout << i+1 << "." << point[i].first << "  " << point[i].second<<"\n";
+        cout << "\n";
 }
 void count_point(){
-    for(int i=0;i<point.size();i++) total_point[point[i].first]+=point[i].second;
+    for(int i=0;i<point.size();i++) {
+        total_point[point[i].first]+=point[i].second;
+        point[i].second = total_point[point[i].first];
+}
+    sort(point.begin(),point.end(),cmp);
 }
 int main()
 {
     int track_num;
-    cout << "歡迎來到f12021模擬器\n";
+    cout << "                                               " << "歡迎來到f12022模擬器\n";
     cout << "輸入賽道個數: ";
     cin >> track_num;
+    cout << "輸入各賽道名稱: ";
     for(int i=1;i<=track_num;i++){
         cin >> track_name[i-1];
     }
@@ -238,11 +340,21 @@ int main()
         race();
         point_system();
         fastest_lap();
-        count_point();
         print_quali();
         print_race();
+        count_point();
+        round_point_system();
         print_driver_standing();
+        match();
         times += 1;
         clear_all();
+        cout<<"----------------------------------\n";
     }
+
 }
+
+/*
+巴林 吉達 澳洲 伊莫拉 邁阿密 巴薩隆納 摩納哥 巴庫
+加拿大 銀石賽道 奧地利 法國 匈牙利 比利時 荷蘭 蒙札
+新加玻 日本 美國 墨西哥 巴西 阿布札比
+*/
